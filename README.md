1fce53f539924b0b59bdb3c61109a920346b9363
Anne Johnson <annekjohnson23@yahoo.com>
2014-10-12 10:51:10 -0400

    Smarter rendering

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ This uses homologous DNA sequences across a few parrot species to generate patte
 ## TODO
 - Get more DNA sequences
 - Put geography data and species name
-- Responsive rendering
-  - Smaller circles on smaller screen sizes
 - Try sequential animation rather than all-at-once
 - Add a play button that fireworks through the different species
 
--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 (function () {
-	var spacer = 130,
-		radiusMultiplier = 18,
-		radiusAdder = 4,
-		nucleotides = "ACGT",
+	var drawVars = {
+			small: {
+				radiusMultiplier: 9,
+				radiusAdder: 4
+			},
+			large: {
+				radiusMultiplier: 18,
+				radiusAdder: 4
+			}
+		},
+		breakpoint = 600,
 		circleDrawTime = 3000,
 		circleRemoveTime = circleDrawTime / 4,
 		redrawTimeout = 200,
-		currBird = "pionus",
-		maxRadius = ((nucleotides.length - 1) + radiusAdder) * radiusMultiplier;
+		margin = 4;
+		nucleotides = "ACGT",
+		currBird = "pionus";
+
+		drawVars.small.maxRadius = ((nucleotides.length - 1) + drawVars.small.radiusAdder) * drawVars.small.radiusMultiplier;
+		drawVars.large.maxRadius = ((nucleotides.length - 1) + drawVars.large.radiusAdder) * drawVars.large.radiusMultiplier;
+
+		drawVars.small.spacer = drawVars.small.maxRadius + margin;
+		drawVars.large.spacer = drawVars.large.maxRadius + margin;
 
 	var birdVis;
 
@@ -26,7 +40,8 @@
 	// param data: array of raw numbers used for computing radii
 	// param colors: array of colors to be applied to shapes
 	// param shouldAnimate: true if user wants animation
-	var writeVis = function(data, colors, shouldAnimate){
+	// param drawVars: a JS object containing radiusAdder, radiusMultiplier, maxRadius, and spacer properties
+	var writeVis = function(data, colors, shouldAnimate, drawVars){
 		if (shouldAnimate) {
 			birdVis.forEach(function(circ) {
 				circ.animate({r: 0}, circleRemoveTime, "linear", function() {
@@ -40,33 +55,40 @@
 			currY = 0,
 			i = 0;
 		while (currY <= screen.height) {
-			var radius = (data[i] + radiusAdder) * radiusMultiplier;
+			var radius = (data[i] + drawVars.radiusAdder) * drawVars.radiusMultiplier;
 			var newCircle;
 			newCircle = birdVis.circle(currX, currY, (shouldAnimate ? 0 : radius));
 			newCircle.attr({fill: colors[data[i]], stroke: colors[data[i]]});
-			currX += radius + spacer;
+			currX += radius + drawVars.spacer;
 			if (shouldAnimate)
 				newCircle.animate({r: radius}, circleDrawTime, "elastic");
 			if (currX > screen.width) {
 				currX = 0;
-				currY += maxRadius + spacer;
+				currY += drawVars.maxRadius + drawVars.spacer;
 			}
 			i++;
 		}
+	};
+
+	// Returns "large" or "small" based on the window size if available - otherwise based
+	// on the screen size
+	var getScreenSize = function() {
+		var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+		return width > breakpoint ? "large" : "small";
 	};
 
 	// Changes the visualization to show the selected bird
 	// Param newBird: the simple string representing the type of bird
 	var changeBird = function(newBird) {
 		currBird = newBird;
-		writeVis(makeData(birds[newBird].seq), birds[newBird].colors, true);
+		writeVis(makeData(birds[newBird].seq), birds[newBird].colors, true, drawVars[getScreenSize()]);
 	};
 
 	// Redraws the visualization after a brief timeout whenever the
 	// screen is resized
 	window.onresize = function() {
 		setTimeout(function() {
-			writeVis(makeData(birds[currBird].seq), birds[currBird].colors, false);
+			writeVis(makeData(birds[currBird].seq), birds[currBird].colors, false, drawVars[getScreenSize()]);
 		}, redrawTimeout);
 	};
 
